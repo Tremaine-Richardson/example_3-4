23ecc366af20dc58afa1fbf6632381528e0be311
Tremaine-Richardson <trey7836@gmail.com>
2024-01-27 18:44:19 +0000

    project 2

--- main.cpp
+++ main.cpp
@@ -1,339 +1,229 @@
 //=====[Libraries]=============================================================
 
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+
 //=====[Defines]===============================================================
 
-#define NUMBER_OF_KEYS                           4
-#define BLINKING_TIME_GAS_ALARM               1000
-#define BLINKING_TIME_OVER_TEMP_ALARM          500
-#define BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM  100
-#define POTENTIOMETER_OVER_TEMP_LEVEL            50
+
 #define TIME_INCREMENT_MS                       10
+#define HEADLIGHT_ON_DELAY 			            2000
+#define HEADLIGHT_OFF_DELAY			            1000
+#define DEBOUNCE_BUTTON_TIME_MS                 40
+
+
+//=====[Declaration of public data types]======================================
+
+
+typedef enum {
+    BUTTON_UP,
+    BUTTON_DOWN,
+    BUTTON_FALLING,
+    BUTTON_RISING
+} buttonState_t;
+
 
 //=====[Declaration and initialization of public global objects]===============
 
-DigitalIn enterButton(BUTTON1);
-DigitalIn gasDetector(D2);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
 
-DigitalOut alarmLed(LED1);
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
+DigitalIn driverSeat(D2);
+DigitalIn ignitionSwitch(D5);
 
-UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+DigitalOut rightHeadlight(D3);
+DigitalOut leftHeadlight(D4);
+DigitalOut engineOnLED(LED2); 
 
-AnalogIn potentiometer(A0);
+AnalogIn lightSensor(A1);
+AnalogIn potentiometer(A0); 
+
 
 //=====[Declaration and initialization of public global variables]=============
 
-bool alarmState    = OFF;
-bool gasDetectorState      = OFF;
-bool overTempDetectorState = OFF;
 
-bool incorrectCode = false;
-bool overTempDetector = OFF;
-
-int numberOfIncorrectCodes = 0;
-int buttonBeingCompared    = 0;
-int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 };
-int buttonsPressed[NUMBER_OF_KEYS] = { 0, 0, 0, 0 };
-int accumulatedTimeAlarm = 0;
+float lightSensorReading = 0.0;
 float potentiometerReading = 0.0;
-float potentiometerReadingScaled = 0.0;
+int accumulatedTimeDelay = 0;
+float sensorReading = 0.0;
+
+bool engineRunning = OFF;
+
+int accumulatedDebounceButtonTime     = 0;
+int numberOfEnterButtonReleasedEvents = 0;
+buttonState_t ignitionSwitchState;
 
 //=====[Declarations (prototypes) of public functions]=========================
+
 
 void inputsInit();
 void outputsInit();
 
-void alarmActivationUpdate();
-void alarmDeactivationUpdate();
+void ignitionStateUpdate();
+void headlightStateUpdate();
 
-void uartTask();
-void availableCommands();
-bool areEqual();
-float celsiusToFahrenheit( float tempInCelsiusDegrees );
-float analogReadingScaledWithTheLM35Formula( float analogReading );
+void ignitionSwitchInit();
+bool ignitionSwitchUpdate();
+
 
 //=====[Main function, the program entry point after power on or reset]========
 
+
 int main()
 {
-    inputsInit();
-    outputsInit();
-    while (true) {
-        alarmActivationUpdate();
-        alarmDeactivationUpdate();
-        uartTask();
-        delay(TIME_INCREMENT_MS);
-    }
+   inputsInit();
+   outputsInit();
+   while (true) {
+       ignitionStateUpdate();
+       headlightStateUpdate();
+       delay(TIME_INCREMENT_MS);
+   }
 }
+
 
 //=====[Implementations of public functions]===================================
 
+
 void inputsInit()
 {
-    gasDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
+   driverSeat.mode(PullDown);
+   ignitionSwitch.mode(PullDown);
 }
+
 
 void outputsInit()
 {
-    alarmLed = OFF;
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
+   rightHeadlight = OFF;
+   leftHeadlight = OFF;
+   engineOnLED = OFF;
 }
 
-void alarmActivationUpdate()
+
+void ignitionStateUpdate()
 {
-    potentiometerReadingScaled = 
-          analogReadingScaledWithTheLM35Formula (potentiometer.read() );
+    bool ignitionSwitch = ignitionSwitchUpdate();
+   if( driverSeat && ignitionSwitch ) {
+     if(!engineRunning){
+         // Start the engine
+         engineOnLED = ON;
+         engineRunning = true;
+     } else{
+         engineOnLED = OFF;
+         engineRunning = false;
+     }
+   }
 
 
-    if ( potentiometerReadingScaled > POTENTIOMETER_OVER_TEMP_LEVEL ) {
-        overTempDetector = ON;
-    } else {
-        overTempDetector = OFF;
-    }
-
-    if( gasDetector) {
-        gasDetectorState = ON;
-        alarmState = ON;
-    }
-    if( overTempDetector ) {
-        overTempDetectorState = ON;
-        alarmState = ON;
-    }
-    if( alarmState ) { 
-        accumulatedTimeAlarm = accumulatedTimeAlarm + TIME_INCREMENT_MS;                      
-    
-        if( gasDetectorState && overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if( gasDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if ( overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_OVER_TEMP_ALARM  ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        }
-    } else{
-        alarmLed = OFF;
-        gasDetectorState = OFF;
-        overTempDetectorState = OFF;
-    }
+   if (engineRunning && !driverSeat){
+       // Keep engine running in absence of driver
+   }
 }
 
-void alarmDeactivationUpdate()
+void headlightStateUpdate() 
 {
-    if ( numberOfIncorrectCodes < 5 ) {
-        if ( aButton && bButton && cButton && dButton && !enterButton ) {
-            incorrectCodeLed = OFF;
-        }
-        if ( enterButton && !incorrectCodeLed && alarmState ) {
-            buttonsPressed[0] = aButton;
-            buttonsPressed[1] = bButton;
-            buttonsPressed[2] = cButton;
-            buttonsPressed[3] = dButton;
-            if ( areEqual() ) {
-                alarmState = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }
-        }
-    } else {
-        systemBlockedLed = ON;
-    }
-}
-
-void uartTask()
-{
-    char receivedChar = '\0';
-    char str[100];
-    int stringLength;
-    if( uartUsb.readable() ) {
-        uartUsb.read( &receivedChar, 1 );
-        switch (receivedChar) {
-        case '1':
-            if ( alarmState ) {
-                uartUsb.write( "The alarm is activated\r\n", 24);
-            } else {
-                uartUsb.write( "The alarm is not activated\r\n", 28);
-            }
-            break;
-
-        case '2':
-            if ( gasDetector ) {
-                uartUsb.write( "Gas is being detected\r\n", 22);
-            } else {
-                uartUsb.write( "Gas is not being detected\r\n", 27);
-            }
-            break;
-
-        case '3':
-            if ( overTempDetector ) {
-                uartUsb.write( "Temperature is above the maximum level\r\n", 40);
-            } else {
-                uartUsb.write( "Temperature is below the maximum level\r\n", 40);
-            }
-            break;
-            
-        case '4':
-            uartUsb.write( "Please enter the code sequence.\r\n", 33 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 ); 
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for ", 41 );
-            uartUsb.write( "not pressed\r\n", 13 );
-            uartUsb.write( "For example, for 'A' = pressed, ", 32 );
-            uartUsb.write( "'B' = pressed, 'C' = not pressed, ", 34);
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            incorrectCode = false;
-
-            for ( buttonBeingCompared = 0;
-                  buttonBeingCompared < NUMBER_OF_KEYS;
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    if ( codeSequence[buttonBeingCompared] != 1 ) {
-                        incorrectCode = true;
+    potentiometerReading = potentiometer.read();
+    lightSensorReading = lightSensor.read();
+    if ( engineRunning ) {
+        if ( potentiometerReading > .3 ) {  
+            if ( potentiometerReading < .6 ) {  //Mode is AUTO
+                 accumulatedTimeDelay = accumulatedTimeDelay + TIME_INCREMENT_MS;
+                if ( lightSensorReading > .2 ) {
+                    if ( lightSensorReading < .5 ) { //Neither Daylight or Dusk
+                        accumulatedTimeDelay = 0;
                     }
-                } else if ( receivedChar == '0' ) {
-                    if ( codeSequence[buttonBeingCompared] != 0 ) {
-                        incorrectCode = true;
+                    else { //Daylight
+                        if ( accumulatedTimeDelay >= HEADLIGHT_OFF_DELAY) {
+                        rightHeadlight = OFF;
+                        leftHeadlight = OFF;
+                        }
                     }
-                } else {
-                    incorrectCode = true;
+                }
+                else { //Dusk
+                    if (accumulatedTimeDelay >= HEADLIGHT_ON_DELAY) {
+                        rightHeadlight = ON;
+                        leftHeadlight = ON;
+                    }
                 }
             }
+            else { //Mode is ON
+                accumulatedTimeDelay = 0;
+                rightHeadlight = ON;
+                leftHeadlight = ON;
+             }
+    }
+    else { //Mode is OFF
+            accumulatedTimeDelay = 0;
+            rightHeadlight = OFF;
+            leftHeadlight = OFF;
+        }
+    }
+    else {
+        rightHeadlight = OFF;
+        leftHeadlight = OFF;
+    }
+}
 
-            if ( incorrectCode == false ) {
-                uartUsb.write( "\r\nThe code is correct\r\n\r\n", 25 );
-                alarmState = OFF;
-                incorrectCodeLed = OFF;
-                numberOfIncorrectCodes = 0;
+void ignitionSwitchInit()
+{
+    if( ignitionSwitch == 1) {
+        ignitionSwitchState = BUTTON_UP;
+    } else {
+        ignitionSwitchState = BUTTON_DOWN;
+    }
+}
+
+
+bool ignitionSwitchUpdate()
+{
+    bool ignitionSwitchReleasedEvent = false;
+    switch( ignitionSwitchState ) {
+
+
+    case BUTTON_UP:
+        if( ignitionSwitch == 0 ) {
+            ignitionSwitchState = BUTTON_FALLING;
+            accumulatedDebounceButtonTime = 0;
+        }
+        break;
+
+
+    case BUTTON_FALLING:
+        if( accumulatedDebounceButtonTime >= DEBOUNCE_BUTTON_TIME_MS ) {
+            if( ignitionSwitch == 0 ) {
+                ignitionSwitchState = BUTTON_DOWN;
             } else {
-                uartUsb.write( "\r\nThe code is incorrect\r\n\r\n", 27 );
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }                
-            break;
-
-        case '5':
-            uartUsb.write( "Please enter new code sequence\r\n", 32 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 );
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for not ", 45 );
-            uartUsb.write( "pressed\r\n", 9 );
-            uartUsb.write( "For example, for 'A' = pressed, 'B' = pressed,", 46 );
-            uartUsb.write( " 'C' = not pressed,", 19 );
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            for ( buttonBeingCompared = 0; 
-                  buttonBeingCompared < NUMBER_OF_KEYS; 
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    codeSequence[buttonBeingCompared] = 1;
-                } else if ( receivedChar == '0' ) {
-                    codeSequence[buttonBeingCompared] = 0;
-                }
+                ignitionSwitchState = BUTTON_UP;
             }
-
-            uartUsb.write( "\r\nNew code generated\r\n\r\n", 24 );
-            break;
- 
-        case 'p':
-        case 'P':
-            potentiometerReading = potentiometer.read();
-            sprintf ( str, "Potentiometer: %.2f\r\n", potentiometerReading );
-            stringLength = strlen(str);
-            uartUsb.write( str, stringLength );
-            break;
-
-        case 'c':
-        case 'C':
-            sprintf ( str, "Temperature: %.2f \xB0 C\r\n", 
-                analogReadingScaledWithTheLM35Formula (
-                    potentiometer.read() ) );
-            stringLength = strlen(str);
-            uartUsb.write( str, stringLength );
-            break;
-
-        case 'f':
-        case 'F':
-            sprintf ( str, "Temperature: %.2f \xB0 F\r\n", 
-                celsiusToFahrenheit( 
-                    analogReadingScaledWithTheLM35Formula (
-                        potentiometer.read() ) ) );
-            stringLength = strlen(str);
-            uartUsb.write( str, stringLength );
-            break;
-
-        default:
-            availableCommands();
-            break;
-
         }
-    }
-}
+        accumulatedDebounceButtonTime = accumulatedDebounceButtonTime + TIME_INCREMENT_MS;
+        break;
 
-void availableCommands()
-{
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '1' to get the alarm state\r\n", 34 );
-    uartUsb.write( "Press '2' to get the gas detector state\r\n", 41 );
-    uartUsb.write( "Press '3' to get the over temperature detector state\r\n", 54 );
-    uartUsb.write( "Press '4' to enter the code sequence\r\n", 38 );
-    uartUsb.write( "Press '5' to enter a new code\r\n", 31 );
-    uartUsb.write( "Press 'P' or 'p' to get potentiometer reading\r\n", 47 );
-    uartUsb.write( "Press 'f' or 'F' to get potentiometer reading in Fahrenheit\r\n", 61 );
-    uartUsb.write( "Press 'c' or 'C' to get potentiometer reading in Celsius\r\n\r\n", 60 );
-}
 
-bool areEqual()
-{
-    int i;
-
-    for (i = 0; i < NUMBER_OF_KEYS; i++) {
-        if (codeSequence[i] != buttonsPressed[i]) {
-            return false;
+    case BUTTON_DOWN:
+        if( ignitionSwitch == 1 ) {
+            ignitionSwitchState = BUTTON_RISING;
+            accumulatedDebounceButtonTime = 0;
         }
+        break;
+
+
+    case BUTTON_RISING:
+        if( accumulatedDebounceButtonTime >= DEBOUNCE_BUTTON_TIME_MS ) {
+            if( ignitionSwitch == 1 ) {
+                ignitionSwitchState = BUTTON_UP;
+                ignitionSwitchReleasedEvent = true;
+            } else {
+                ignitionSwitchState = BUTTON_DOWN;
+            }
+        }
+        accumulatedDebounceButtonTime = accumulatedDebounceButtonTime +
+                                        TIME_INCREMENT_MS;
+        break;
+
+
+    default:
+        ignitionSwitchInit();
+        break;
     }
-
-    return true;
-}
-
-float analogReadingScaledWithTheLM35Formula( float analogReading )
-{
-    return ( analogReading * 3.3 / 0.01 );
-}
-
-float celsiusToFahrenheit( float tempInCelsiusDegrees )
-{
-    return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
+    return ignitionSwitchReleasedEvent;
 }
